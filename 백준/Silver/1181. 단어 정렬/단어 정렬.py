8837ddcbe80144a0f12323a11b7b43ef925d17be
Choi JaeHyuk <113301308+ChoiJHy@users.noreply.github.com>
2024-09-11 00:15:45 +0900

    [Silver V] Title: 단어 정렬, Time: 588 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,19 +1,49 @@
-def merge_sort(words):
-    if len(words) <= 1:
-        return words
+def MergeSort(arr):
 
-    # 중간 지점 설정
-    mid = len(words) // 2
-    left = merge_sort(words[:mid])  # 왼쪽 반 정렬
-    right = merge_sort(words[mid:])  # 오른쪽 반 정렬
+    if len(arr) <= 1:
+        return arr
+    
+    # 리스트의 길이가 2인경우는 나누지 않고 바로 정렬해서 리턴
+    # if len(arr) == 2:
+    #     return [arr[0], arr[1]] if len(arr[0]) < len(arr[1]) else [arr[1], arr[0]]
+    
+    # 리스트를 절반으로 나눈다.
+    middle = len(arr) // 2
+    left_arr = MergeSort(arr[:middle]) # 0부터 middle 이전까지
+    right_arr = MergeSort(arr[middle:]) # middle부터 마지막 인덱스까지 
 
-    return merge(left, right)
+    return Merge(left_arr, right_arr)
 
-def merge(left, right):
+def Merge(left, right):
     result = []
     i = j = 0
 
-    # 두 리스트를 비교하며 하나로 합친다.
+    # # 오름차순으로 정렬하면서 병합을 진행한다.
+    # while i < len(left) and j < len(right):
+    #     if len(left[i]) < len(right[j]):
+    #         if not result or result[-1] != left[i]: # 리스트가 비어있거나 맨 마지막 들어간 요소와 중복이 아닐 때
+    #             result.append(left[i])
+    #         i += 1
+    #     elif len(left[i]) > len(right[j]):
+    #         if not result or result[-1] != right[j]:
+    #             result.append(right[j])
+    #         j += 1
+    #     else: # 길이가 같다면 사전순으로 정렬하고 중복을 확인한다.
+    #         if left[i] < right[j]:
+    #             if not result or result[-1] != left[i]:
+    #                 result.append(left[i])
+    #             i += 1
+    #         elif left[i] > right[j]:
+    #             if not result or result[-1] != right[j]:
+    #                 result.append(right[j])
+    #             j += 1
+    #         else: # 중복된 단어라면 한 번만 추가한다.
+    #             if not result or result[-1] != left[i]:
+    #                 result.append(left[i])
+    #             i += 1
+    #             j += 1
+
+     # 두 리스트를 비교하며 하나로 합친다.
     while i < len(left) and j < len(right):
         if len(left[i]) < len(right[j]) or (len(left[i]) == len(right[j]) and left[i] < right[j]):
             result.append(left[i])
@@ -21,23 +51,52 @@ def merge(left, right):
         else:
             result.append(right[j])
             j += 1
+    
+    # 리스트에 남아있는 요소들 추가
+    # while i < len(left):
+    #     if not result or result[-1] != left[i]:
+    #         result.append(left[i])
+    #     i += 1
+    
+    # while j < len(right):
+    #     if not result or result[-1] != right[j]:
+    #         result.append(right[j])
+    #     j += 1
 
-    # 남아 있는 원소를 추가
     result.extend(left[i:])
     result.extend(right[j:])
 
+    
     return result
 
-# 입력 받기
+
+def IsLowerAlpha(word):
+
+    if len(word) == 0:
+        return False
+    for char in word:
+        if not ('a' <= char <= 'z'):
+            return False
+    
+    return True
+
+
+# 단어 갯수 입력
 n = int(input())
-words = set()  # 중복을 제거하기 위해 set 사용
+words = set()
+# words = [input() for _ in range(n)] # n개의 단어 입력
+# words = []
 
 for _ in range(n):
     words.add(input().strip())
 
-# 리스트로 변환 후 병합 정렬 호출
-sorted_words = merge_sort(list(words))
+# for _ in range(n):
+#     words.append(input())
+#     # if IsLowerAlpha(word):
+#     #     words.append(word)
+    
 
-# 출력
-for word in sorted_words:
+sorted_list = MergeSort(list(words))
+
+for word in sorted_list:
     print(word)
