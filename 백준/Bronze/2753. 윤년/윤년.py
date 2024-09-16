079a513152e712de0c47a188e52a660a4493738c
Choi JaeHyuk <113301308+ChoiJHy@users.noreply.github.com>
2024-09-16 16:01:57 +0900

    [Bronze V] Title: 윤년, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,8 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 
 if (n % 4 == 0 and n % 100 != 0) or n % 400 == 0:
-    print(1)
+    sys.stdout.write('1\n')
 else:
-    print(0)
+    sys.stdout.write('0\n')
