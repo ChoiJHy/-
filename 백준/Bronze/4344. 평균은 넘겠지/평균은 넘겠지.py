ae1d3a786d1ce408bd3a77cb36a8a3062c9259ba
Choi JaeHyuk <113301308+ChoiJHy@users.noreply.github.com>
2024-09-18 12:51:42 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for _ in range(n):
+    scores = list(map(int, input().split()))
+    average = sum(scores[1:]) / scores[0]
+
+    count = 0
+    for i in scores[1:]:
+        if i > average:
+            count += 1
+    
+    print('%.3f' %((count / scores[0]) * 100) + '%')
