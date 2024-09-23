8f3d7d49bb6b736924d38c440a56856598f72fa9
Choi JaeHyuk <113301308+ChoiJHy@users.noreply.github.com>
2024-09-23 17:41:02 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,45 +1,28 @@
-import sys
-input = sys.stdin.readline
+A = int(input())
+B = list(map(int,input().split()))
+C = list(map(int,input().split()))
 
-def solution(add, sub, mul, div):
-    i = 0 # numbers에 접근하기 위한 인덱스
-    stack = [(numbers[0], add, sub, mul, div, i)] # 스택을 초기화함(path, 연산자들)
-    max_result = -int(1e9)
-    min_result = int(1e9)
+Maximum = -1e9
+Minimum = 1e9 
 
-    while stack:
-        result, add_left, sub_left, mul_left, div_left, index = stack.pop()
 
-        # 모든 연산자를 사용한 경우
-        if 0 == add_left and 0 == sub_left and 0 == mul_left and 0 == div_left:
-            max_result = max(max_result, result)
-            min_result = min(min_result, result)
-            continue
+def solution(depth,count,add,sub,mul,div):
+     global Maximum,Minimum
+     if depth == A:
+          Maximum = max(count,Maximum)
+          Minimum = min(count,Minimum)
 
-        # 덧셈 연산자를 사용할 수 있는 경우
-        if 0 < add_left:
-            stack.append((result + numbers[index+1], add_left - 1, sub_left, mul_left, div_left, index + 1))
+          return
+     if add:
+          solution(depth+1,count+B[depth],add-1,sub,mul,div)
+     if sub:
+          solution(depth+1,count-B[depth],add,sub-1,mul,div)
+     if mul:
+          solution(depth+1,count*B[depth],add,sub,mul-1,div)
+     if div:
+          solution(depth+1,int(count/B[depth]),add,sub,mul,div-1)
 
-        # 뺄샘 연산자를 사용할 수 있는 경우
-        if 0 < sub_left:
-            stack.append((result - numbers[index+1], add_left, sub_left - 1, mul_left, div_left, index + 1))
-        
-        # 곱셈 연산자를 사용할 수 있는 경우
-        if 0 < mul_left:
-            stack.append((result * numbers[index+1], add_left, sub_left, mul_left - 1, div_left, index + 1))
 
-        # 나눗셈 연산자를 사용할 수 있는 경우
-        if 0 < div_left:
-            if 0 > result:
-                stack.append((-(-result // numbers[index+1]), add_left, sub_left, mul_left, div_left - 1, index + 1))
-            else:
-                stack.append((result // numbers[index+1], add_left, sub_left, mul_left, div_left - 1, index + 1))
-
-    print(max_result)
-    print(min_result)
-    
-n = int(input())
-numbers = list(map(int, input().split()))
-operators = list(map(int, input().split()))
-
-solution(operators[0], operators[1], operators[2], operators[3])
+solution(1,B[0],C[0],C[1],C[2],C[3])
+print(Maximum)
+print(Minimum)
