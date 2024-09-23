9cc37843fcb741d24e966fc4a7383b980110a5f6
Choi JaeHyuk <113301308+ChoiJHy@users.noreply.github.com>
2024-09-23 17:29:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,72 +1,45 @@
 import sys
 input = sys.stdin.readline
 
-def Make_Operator_Combinations(add, sub, mul, div):
-    stack = [([], add, sub, mul, div)] # 스택을 초기화함(path, 연산자들)
-    operator_combinations = [] # 최종결과를 저장할 리스트
+def solution(add, sub, mul, div):
+    i = 0 # numbers에 접근하기 위한 인덱스
+    stack = [(numbers[0], add, sub, mul, div, i)] # 스택을 초기화함(path, 연산자들)
+    max_result = -int(1e9)
+    min_result = int(1e9)
 
     while stack:
-        path, add_left, sub_left, mul_left, div_left = stack.pop()
+        result, add_left, sub_left, mul_left, div_left, index = stack.pop()
 
         # 모든 연산자를 사용한 경우
         if 0 == add_left and 0 == sub_left and 0 == mul_left and 0 == div_left:
-            operator_combinations.append(path)
+            max_result = max(max_result, result)
+            min_result = min(min_result, result)
             continue
 
         # 덧셈 연산자를 사용할 수 있는 경우
         if 0 < add_left:
-            stack.append((path + ['+'], add_left - 1, sub_left, mul_left, div_left))
+            stack.append((result + numbers[index+1], add_left - 1, sub_left, mul_left, div_left, index + 1))
 
         # 뺄샘 연산자를 사용할 수 있는 경우
         if 0 < sub_left:
-            stack.append((path + ['-'], add_left, sub_left - 1, mul_left, div_left))
+            stack.append((result - numbers[index+1], add_left, sub_left - 1, mul_left, div_left, index + 1))
         
         # 곱셈 연산자를 사용할 수 있는 경우
         if 0 < mul_left:
-            stack.append((path + ['*'], add_left, sub_left, mul_left - 1, div_left))
+            stack.append((result * numbers[index+1], add_left, sub_left, mul_left - 1, div_left, index + 1))
 
         # 나눗셈 연산자를 사용할 수 있는 경우
         if 0 < div_left:
-            stack.append((path + ['/'], add_left, sub_left, mul_left, div_left - 1))
+            if 0 > result:
+                stack.append((-(-result // numbers[index+1]), add_left, sub_left, mul_left, div_left - 1, index + 1))
+            else:
+                stack.append((result // numbers[index+1], add_left, sub_left, mul_left, div_left - 1, index + 1))
+
+    print(max_result)
+    print(min_result)
     
-    return operator_combinations
-
-
 n = int(input())
 numbers = list(map(int, input().split()))
 operators = list(map(int, input().split()))
 
-operator_combinations = Make_Operator_Combinations(operators[0], operators[1], operators[2], operators[3])
-
-# print(len(operator_combinations))
-# for ops in operator_combinations:
-#     print(ops)
-
-max_result = -int(1e9)
-min_result = int(1e9)
-
-# 연산자 조합의 수만큼 계산
-for operator in operator_combinations:
-    result = numbers[0]
-    if ['-', '/', '+', '+', '*'] == operator:
-        a = 0
-    for i in range(1,n): 
-        if '+' == operator[i-1]:
-            result += numbers[i]
-        elif '-' == operator[i-1]:
-            result -= numbers[i]
-        elif '*' == operator[i-1]:
-            result *= numbers[i]
-        elif '/' == operator[i-1]:
-            if 0 > result:
-                result = -(-result // numbers[i])
-            else:
-                result //= numbers[i]
-    
-    max_result = max(max_result, result)
-    min_result = min(min_result, result)
-
-print(max_result)
-print(min_result)
-
-
+solution(operators[0], operators[1], operators[2], operators[3])
