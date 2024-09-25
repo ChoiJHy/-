c634caeaf4fc4fc5294e8df7c58a4dda8c61fedd
Choi JaeHyuk <113301308+ChoiJHy@users.noreply.github.com>
2024-09-25 13:51:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 216 ms, Memory: 52828 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -19,22 +19,44 @@ for _ in range(m):
 start, end = map(int, input().split())
 
 costs[start - 1] = 0
-heap = []
-heapq.heappush(heap,(0, start-1))
 
-while heap:
-    cost, current_node = heapq.heappop(heap)
+#####################################
+#       힙을 사용해서 만드는 방법 
+#######################################
+# heap = []
+# heapq.heappush(heap,(0, start-1))
 
-    # if costs[current_node] < cost and visited[current_node]:
-    #     continue
-    if costs[current_node] < cost :
+# while heap:
+#     cost, current_node = heapq.heappop(heap)
+
+#     if costs[current_node] < cost and visited[current_node]:
+#         continue
+#     # if costs[current_node] < cost :
+#     #     continue
+
+#     visited[current_node] = True
+#     for next_node, next_cost in graph[current_node]:
+#         total_cost = cost + next_cost
+#         if total_cost < costs[next_node]: # 인접노드를 거쳐가는 비용이 원래 비용보다 쌀경우
+#             costs[next_node] = total_cost # 최소비용갱신
+#             heapq.heappush(heap, (total_cost, next_node))
+
+
+#####################################
+#      우선순위 큐를 사용해서 만들기
+#######################################
+queue = deque([[0, start - 1]])
+
+while queue:
+    cost, current_node = queue.popleft()
+
+    if costs[current_node] < cost:
         continue
 
-    # visited[current_node] = True
     for next_node, next_cost in graph[current_node]:
         total_cost = cost + next_cost
-        if total_cost < costs[next_node]: # 인접노드를 거쳐가는 비용이 원래 비용보다 쌀경우
-            costs[next_node] = total_cost # 최소비용갱신
-            heapq.heappush(heap, (total_cost, next_node))
-
+        if total_cost < costs[next_node]:
+            costs[next_node] = total_cost
+            queue.append([total_cost, next_node])
+            
 print(costs[end-1])
