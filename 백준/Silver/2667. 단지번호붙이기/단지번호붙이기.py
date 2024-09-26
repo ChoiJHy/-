4a4271d1b2eac38565a95f6a771b2296ec3105ac
Choi JaeHyuk <113301308+ChoiJHy@users.noreply.github.com>
2024-09-26 16:58:01 +0900

    [Silver I] Title: 단지번호붙이기, Time: 52 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,50 +1,50 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 
+# 입력
 n = int(input())
-MAP = []
-ans = []
+# 그래프 생성
+graph = [list(map(int, input().strip())) for _ in range(n)]
+# 방문리스트
+visited = [[False] * n for _ in range(n)]
+# 4방향 탐색 키
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for _ in range(n):
-    MAP.append(list(map(int, input().rstrip())))
+# BFS---------------------------------------------------------------------------------------------------------
+def BFS(x, y):
+    queue = deque([(x, y)])
+    visited[x][y] = True
+    count = 1
 
-# for i in range(n):
-#     print(MAP[i])
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True 
+                queue.append((nx, ny))
+                count += 1
 
+    return count
+# ------------------------------------------------------------------------------------------------------------
 
-# bfs에 활용할 큐 생성
-queue = deque()
-# 집 개수를 샐 변수
-count = 0
-# 상 하 좌 우
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+# 결과값
+result = []
 
-for y in range(n):
-    for x in range(n):
-        if 1 == MAP[y][x]: # 맵을 돌다 집을 발견했다면 
-            queue.append([y, x]) # 큐에 넣고 
-            MAP[y][x] = 0 # 다시 방문하지 않도록 변경 
-            count += 1 # 집 개수 증가
-            
-            # 큐가 빌 때까지 탐색 진행
-            while queue:
-                cy, cx = queue.popleft()
-                for i in range(4):
-                    ny, nx = cy + dy[i], cx + dx[i]
+# 탐색
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 and not visited[i][j]:
+            result.append(BFS(i, j))
 
-                    #다음 위치가 범위를 벗어나지 않으며 집이 있는경우
-                    if 0 <= ny < n and 0 <= nx < n and 1 == MAP[ny][nx]:
-                        MAP[ny][nx] = 0 # 해당 집은 방문처리
-                        count += 1 # 집 개수를 늘려주고
-                        queue.append([ny, nx]) # 큐에 넣어준다.
-            # 탐색이 끝나고 나왔다면 
-            ans.append(count) # 리스트에 결과값을 넣어주고
-            count = 0 # 카운트 초기화
+# 오름차순 정렬
+result.sort()
 
-ans.sort() # 결과를 오름차순으로 출력하기 위해서 정렬
+# 단지의 개수
+print(len(result))
 
-print(len(ans))
-for i in ans:
-    print(i)
+# 정렬한 단지별 아파트 개수
+for apartment_c in result:
+    print(apartment_c)
